ab55bba8aaa4a7a0b82bdf46886ca15f7d1e68fd
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2017-05-01 20:19:51 -0400

    added starter files

---
+++ Chat/src/com/chat/demo/EchoEndPoint.java
@@ -0,0 +1,14 @@
+package com.chat.demo;
+
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
+import javax.websocket.Session;
+
+public class EchoEndPoint extends Endpoint {
+
+	@Override
+	public void onOpen(Session session, EndpointConfig endpointConfig) {
+		
+	}
+
+}

2ab9a33ef2a5c71bc66a6c1497fe39c923fbecd8
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2017-05-02 23:38:07 -0400

    onOpen and sending messages

--- Chat/src/com/chat/demo/EchoEndPoint.java
+++ Chat/src/com/chat/demo/EchoEndPoint.java
@@ -1,14 +1,40 @@
 package com.chat.demo;
 
+import java.io.IOException;
+
 import javax.websocket.Endpoint;
 import javax.websocket.EndpointConfig;
+import javax.websocket.MessageHandler;
+import javax.websocket.RemoteEndpoint;
 import javax.websocket.Session;
 
 public class EchoEndPoint extends Endpoint {
 
 	@Override
 	public void onOpen(Session session, EndpointConfig endpointConfig) {
-		
+		RemoteEndpoint.Basic remoteEndpointBasic = session.getBasicRemote();
+		session.addMessageHandler(new EchoMessageHandler(remoteEndpointBasic));
 	}
+	
+	public static class EchoMessageHandler implements MessageHandler.Whole<String> {
+		
+		private final RemoteEndpoint.Basic remoteEndpointBasic;
+		
+		private EchoMessageHandler(RemoteEndpoint.Basic remoteEndpointBasic) {
+			this.remoteEndpointBasic = remoteEndpointBasic;
+		}
+		
+		@Override
+		public void onMessage(String message) {
+			try {
+				if(remoteEndpointBasic != null) {
+					remoteEndpointBasic.sendText(message);
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
 
+	}
+	
 }
